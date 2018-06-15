f3df41dea97d39755d50abedf892a47003f5c290
T. Jameson Little <t.jameson.little@gmail.com>
2018-06-14 23:28:27 -0600

    Added Github badge

--- src/App.js
+++ src/App.js
@@ -108,6 +108,12 @@ class App extends Component {
 
         <label>Other ({other.length}):</label>
         <IssueList issues={other} />
+
+        <br />
+
+        <a href="https://github.com/beatgammit/jira-time-web">
+          <img style={{position: 'absolute', top: 0, right: 0, border: 0}} src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub" />
+        </a>
       </div>
     );
   }
